48ba86f687c6754f0c0a780bafd9df63730eb512
Matt Susskind <susskind.matt@gmail.com>
2019-07-23 16:49:20 -0700

    Game works

--- sliding.py
+++ sliding.py
@@ -3,13 +3,17 @@ A = 4
 B = 2
 P = 1
 
+DIR_LEFT = (0, -1)
+DIR_RIGHT = (0, 1)
+DIR_UP = (-1, 0)
+DIR_DOWN = (1, 0)
 
 class Puzzle:
 
 	def __init__(self, board):
 		
 		if not isinstance(board, list) or len(board) == 0:
-			raise ValueError("Give me a board, doofus")
+		 	raise ValueError("Give me a board, doofus")
 		self.playerPosition = (-1,-1)
 		self.board = board
 		for row, line in enumerate(self.board):
@@ -25,16 +29,66 @@ class Puzzle:
 	def prettyPrint(self):
 		for line in self.board:
 			print line
+		print 
 
+	def outOfBounds(self, position):
+	 	if (position[0] < 0 or position[0] >= self.rows) or (position[1] < 0 or position[1] >= self.cols):
+	 		return True
+
+	def isOpen(self, position):
+		r = position[0]
+		c = position[1]
+		return not self.outOfBounds(position) and (self.board[r][c] == 0 or  self.board[r][c] == A)
+
+	def moveObject(self, origPosition, direction, isPlayer = False):
+		currentCell = origPosition
+		adjCell = (currentCell[0] + direction[0], currentCell[1] + direction[1])
+		while self.isOpen(adjCell):
+			currentCell = adjCell
+			if (self.board[currentCell[0]][currentCell[1]] == A and isPlayer):
+				print "You Win!"
+				self.board[currentCell[0]][currentCell[1]] = self.board[origPosition[0]][origPosition[1]]
+				self.board[origPosition[0]][origPosition[1]] = 0
+				return True
+			adjCell = (currentCell[0] + direction[0], currentCell[1] + direction[1])
+		if not currentCell == origPosition:
+			self.board[currentCell[0]][currentCell[1]] = self.board[origPosition[0]][origPosition[1]]
+			self.board[origPosition[0]][origPosition[1]] = 0
+		if (isPlayer):
+			self.playerPosition = currentCell
+
+
+	def movePlayer(self, direction):
+		adjCell = (self.playerPosition[0] + direction[0], self.playerPosition[1] + direction[1])
+	 	if self.outOfBounds(adjCell):
+	 		return
+	 	elif self.board[adjCell[0]][adjCell[1]] == B:
+	 	 	self.moveObject(adjCell, direction)
+	 	else:
+	 		self.moveObject(self.playerPosition, direction, True)
 
 print
 
-puzzle1 = [0 , 0, 0, A ,0], \
+puzzle1 = [[0 , 0, 0, A ,0], \
 		  [X , 0, 0, 0 ,0], \
 		  [P , B, 0, 0 ,0], \
 		  [X , 0, 0, X ,0], \
-		  [0 , 0, 0, 0 ,0]
+		  [0 , 0, 0, 0 ,0]]
 
 p1 = Puzzle(puzzle1)
 p1.prettyPrint()
-print p1.playerPosition
+
+# push block to the right
+p1.movePlayer(DIR_RIGHT)
+p1.prettyPrint()
+
+#move player to the right
+p1.movePlayer(DIR_RIGHT)
+p1.prettyPrint()
+
+#move player up to the goal
+p1.movePlayer(DIR_UP)
+p1.prettyPrint()
+
+
+
